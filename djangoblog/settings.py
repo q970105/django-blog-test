ab4941d27dd74f83d87c03ad041341fff46ca611
yabee <q970105@gmail.com>
2022-09-06 04:01:03 +0800

    test

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -34,7 +34,7 @@ DEBUG = False
 TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['*', '127.0.0.1', 'example.com','django-blog-test.azurewebsites.net']
+ALLOWED_HOSTS = ['*', '127.0.0.1', 'example.com']
 # django 4.0新增配置
 CSRF_TRUSTED_ORIGINS = ['http://example.com']
 # Application definition
